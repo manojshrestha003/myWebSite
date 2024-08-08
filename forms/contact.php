3264bce2d4175117a1e21fb01495a730a1643888
Manoj Shrestha <manoj.shrestha2081@gmail.com>
2024-08-08 21:32:29 +0545

    latest commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
  
-  // Replace contact@example.com with your real receiving email address
+  
   $receiving_email_address = 'manoj.shrestha2081@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
